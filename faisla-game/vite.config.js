5204734e6227df772f1969e159a30600dd150f67
UK <ukstudies27@gmail.com>
2026-04-14 14:31:39 +0530

    added deployment configs

--- faisla-game/vite.config.js
+++ faisla-game/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/Faisla_game/',   // replace with your actual repo name
   preview: {
     allowedHosts: 'all',
   },
